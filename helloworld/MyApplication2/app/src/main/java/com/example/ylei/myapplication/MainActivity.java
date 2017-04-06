f208e1a9f8ed38721683e6d4a29e2deeb75c695f
Lei <yang_lei@mcafee.com>
2017-04-06 16:59:58 +0800

    a+b

--- helloworld/MyApplication2/app/src/main/java/com/example/ylei/myapplication/MainActivity.java
+++ helloworld/MyApplication2/app/src/main/java/com/example/ylei/myapplication/MainActivity.java
@@ -2,12 +2,38 @@ package com.example.ylei.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.widget.*;
+import android.view.View;
 public class MainActivity extends AppCompatActivity {
+
+    Button btnCalc;
+    EditText txtA;
+    EditText txtB;
+    TextView txtResult;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCalc = (Button) findViewById(R.id.btnCalc);
+        txtA = (EditText) findViewById(R.id.txtA);
+        txtB = (EditText) findViewById(R.id.txtB);
+        txtResult = (TextView) findViewById(R.id.txtResult);
+
+        btnCalc.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                int a=Integer.parseInt(txtA.getText().toString());
+                int b=Integer.parseInt(txtB.getText().toString());
+                int c=a+b;
+                txtResult.setText(String.valueOf(c));
+
+            }
+        });
+
     }
 }
