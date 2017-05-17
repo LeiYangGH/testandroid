9339f087cefc22dd3f419fc0e8d67612814b3099
Lei <yang_lei@mcafee.com>
2017-05-17 16:22:39 +0800

    can get json

--- KD100/app/src/main/java/com/example/ylei/kd100/CallAPI.java
+++ KD100/app/src/main/java/com/example/ylei/kd100/CallAPI.java
@@ -10,6 +10,7 @@ import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
 import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
@@ -25,13 +26,28 @@ public class CallAPI extends AsyncTask<String, String, String> {
     @Override
     protected String doInBackground(String... params) {
         HashMap<String, String> ps=new HashMap<String, String>();
-                /*ps.put("type", "jd");
+                //*
+                ps.put("type", "jd");
                 ps.put("postid", "50460919160");
-                ps.put("id", "1");*/
-        // String webcontent=performPostCall("http://www.kuaidi100.com/query",ps);
+                ps.put("id", "1");
+                //*/
+        String webcontent=performPostCall("http://www.kuaidi100.com/query",ps);
         // String webcontent=performPostCall("http://www.baidu.com/",null);
-        String webcontent=performPostCall("http://www.baidu.com/",ps);
+
+        //String webcontent=performPostCall("http://www.baidu.com/",ps);
+        //String webcontent=performPostCall("http://www.sina.com.cn/",ps);
         return webcontent;
+    }
+
+    private static URLConnection reload(URLConnection uc) throws Exception {
+
+        HttpURLConnection huc = (HttpURLConnection) uc;
+
+        if (huc.getResponseCode() == HttpURLConnection.HTTP_MOVED_TEMP
+                || huc.getResponseCode() == HttpURLConnection.HTTP_MOVED_PERM)// 302, 301
+            return reload(new URL(huc.getHeaderField("location")).openConnection());
+
+        return uc;
     }
 
     public String  performPostCall(String requestURL,
@@ -52,18 +68,20 @@ public class CallAPI extends AsyncTask<String, String, String> {
             conn.setDoInput(true);
             conn.setDoOutput(true);
 
-
+///*
             OutputStream os = conn.getOutputStream();
             BufferedWriter writer = new BufferedWriter(
                     new OutputStreamWriter(os, "UTF-8"));
-            //response+="getPostDataString0";
-            //writer.write(getPostDataString(postDataParams));
-            //response+="getPostDataString1";
+            writer.write(getPostDataString(postDataParams));
             writer.flush();
             writer.close();
             os.close();
+            //*/
+            conn = (HttpURLConnection)reload(conn);////////
+
             int responseCode=conn.getResponseCode();
 
+            response+="responseCode="+String.valueOf(responseCode)+"=";
             //if (responseCode == HttpsURLConnection.HTTP_OK) {
                 if (true) {
                 response+="responseCode";
