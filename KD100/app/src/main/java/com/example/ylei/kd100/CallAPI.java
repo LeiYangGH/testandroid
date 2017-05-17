04ba266e3a0ede0cd76c82790a6c2d408dd1557e
Lei <yang_lei@mcafee.com>
2017-05-17 15:18:44 +0800

    stuck on network

---
+++ KD100/app/src/main/java/com/example/ylei/kd100/CallAPI.java
@@ -0,0 +1,105 @@
+package com.example.ylei.kd100;
+
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.net.ssl.HttpsURLConnection;
+
+/**
+ * Created by YLei on 2017-05-17.
+ */
+
+public class CallAPI extends AsyncTask<String, String, String> {
+
+    @Override
+    protected String doInBackground(String... params) {
+        HashMap<String, String> ps=new HashMap<String, String>();
+                /*ps.put("type", "jd");
+                ps.put("postid", "50460919160");
+                ps.put("id", "1");*/
+        // String webcontent=performPostCall("http://www.kuaidi100.com/query",ps);
+        // String webcontent=performPostCall("http://www.baidu.com/",null);
+        String webcontent=performPostCall("http://www.baidu.com/",ps);
+        return webcontent;
+    }
+
+    public String  performPostCall(String requestURL,
+                                   HashMap<String, String> postDataParams) {
+
+        URL url;
+
+        String response = "";
+        response+="try";
+        try {
+            url = new URL(requestURL);
+            //response+=requestURL;
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(15000);
+            conn.setConnectTimeout(15000);
+            conn.setRequestMethod("GET");
+            //conn.setRequestMethod("POST");
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+
+
+            OutputStream os = conn.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(
+                    new OutputStreamWriter(os, "UTF-8"));
+            //response+="getPostDataString0";
+            //writer.write(getPostDataString(postDataParams));
+            //response+="getPostDataString1";
+            writer.flush();
+            writer.close();
+            os.close();
+            int responseCode=conn.getResponseCode();
+
+            //if (responseCode == HttpsURLConnection.HTTP_OK) {
+                if (true) {
+                response+="responseCode";
+                String line;
+                BufferedReader br=new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                while ((line=br.readLine()) != null) {
+                    response+=line;
+                }
+            }
+            else {
+                response+="!responseCode";
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            response+=e.getMessage();
+            response+=e.toString();
+        }
+
+        return response;
+    }
+
+    private String getPostDataString(HashMap<String, String> params) throws UnsupportedEncodingException {
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+        for(Map.Entry<String, String> entry : params.entrySet()){
+            if (first)
+                first = false;
+            else
+                result.append("&");
+
+            result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+        }
+
+        return result.toString();
+    }
+}
--- KD100/app/src/main/java/com/example/ylei/kd100/MainActivity.java
+++ KD100/app/src/main/java/com/example/ylei/kd100/MainActivity.java
@@ -1,17 +1,34 @@
 package com.example.ylei.kd100;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Bundle;
 import android.widget.*;
 import android.view.View;
 import android.content.SharedPreferences;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.*;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+import javax.net.ssl.HttpsURLConnection;
+
 public class MainActivity extends AppCompatActivity {
 
     Button btnQuery;
     EditText txtNO;
     TextView txtMsg;
-
+String msg="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,17 +45,33 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-               String no= txtNO.getText().toString();
-                txtMsg.setText(no);
+                btnQuery.setEnabled(false);
+                String no= txtNO.getText().toString();
+
 
                 SharedPreferences sharedPref =  getPreferences(MODE_PRIVATE);
                 SharedPreferences.Editor editor = sharedPref.edit();
                 editor.putString("no", no);
                 editor.commit();
 
+                String webcontent= null;
+                try {
+                    webcontent = (new CallAPI()).execute("","","").get();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                }
 
+
+                webcontent+="done in main";
+                txtMsg.setText(webcontent);
+                btnQuery.setEnabled(true);
+                //txtNO.setText(msg);
             }
         });
 
     }
+
+
 }
