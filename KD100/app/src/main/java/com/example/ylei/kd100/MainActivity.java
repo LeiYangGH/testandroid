289429f1e9ecdee50ef530cfae833d50d8c7fec1
Lei <yang_lei@mcafee.com>
2017-05-17 11:12:25 +0800

    remember no:SharedPreferences

--- KD100/app/src/main/java/com/example/ylei/kd100/MainActivity.java
+++ KD100/app/src/main/java/com/example/ylei/kd100/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.os.Bundle;
 import android.widget.*;
 import android.view.View;
+import android.content.SharedPreferences;
 public class MainActivity extends AppCompatActivity {
 
     Button btnQuery;
@@ -19,12 +20,23 @@ public class MainActivity extends AppCompatActivity {
         txtNO = (EditText) findViewById(R.id.txtNO);
         txtMsg = (TextView) findViewById(R.id.txtMsg);
 
+        SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+        String no = prefs.getString("no", "112233");
+        txtMsg.setText(no);
+
         btnQuery.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
                String no= txtNO.getText().toString();
                 txtMsg.setText(no);
+
+                SharedPreferences sharedPref =  getPreferences(MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putString("no", no);
+                editor.commit();
+
+
             }
         });
 
