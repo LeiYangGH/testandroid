3fd4a63d0f762307e6c0a17a6e73ae1c60cefc1c
Lei <yang_lei@mcafee.com>
2017-05-17 10:52:49 +0800

    layout arrange

--- KD100/app/src/main/java/com/example/ylei/kd100/MainActivity.java
+++ KD100/app/src/main/java/com/example/ylei/kd100/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.ylei.kd100;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.os.Bundle;
+import android.widget.*;
+import android.view.View;
 public class MainActivity extends AppCompatActivity {
+
+    Button btnQuery;
+    EditText txtNO;
+    TextView txtMsg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnQuery = (Button) findViewById(R.id.btnQuery);
+        txtNO = (EditText) findViewById(R.id.txtNO);
+        txtMsg = (TextView) findViewById(R.id.txtMsg);
+
+        btnQuery.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+               String no= txtNO.getText().toString();
+                txtMsg.setText(no);
+            }
+        });
+
     }
 }
