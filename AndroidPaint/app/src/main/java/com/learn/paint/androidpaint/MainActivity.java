cf449f3a10827fd48d7494b2555c607685d27480
Lei <yang_lei@mcafee.com>
2017-05-26 11:00:52 +0800

    drawType == 1 drawline

--- AndroidPaint/app/src/main/java/com/learn/paint/androidpaint/MainActivity.java
+++ AndroidPaint/app/src/main/java/com/learn/paint/androidpaint/MainActivity.java
@@ -61,35 +61,70 @@ public class MainActivity extends AppCompatActivity {
             super.onDraw(canvas);
 
             canvas.drawBitmap( mBitmap, 0, 0, mBitmapPaint);
-            canvas.drawPath( mPath,  mPaint);
+
+
+                                  if(drawType==1) {
+                                    //  mCanvas.drawLine(0, 0, 100, 100, mPaint);
+                                      mCanvas.drawLine(mX0, mY0, mX, mY, mPaint);
+                                  }
+                                                else if(drawType==4) {
+                                      canvas.drawPath( mPath,  mPaint);
+            }
         }
 
         private float mX, mY;
+        private float mX0, mY0;
         private static final float TOUCH_TOLERANCE = 4;
 
         private void touch_start(float x, float y) {
-            mPath.reset();
-            mPath.moveTo(x, y);
-            mX = x;
-            mY = y;
+
+            if(drawType==1) {
+                mX0 = x;
+                mY0 = y;
+            }
+            else if(drawType==4) {
+                mPath.reset();
+                mPath.moveTo(x, y);
+                mX = x;
+                mY = y;
+            }
+
+
         }
 
         private void touch_move(float x, float y) {
-            float dx = Math.abs(x - mX);
-            float dy = Math.abs(y - mY);
-            if (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
-                mPath.quadTo(mX, mY, (x + mX)/2, (y + mY)/2);
-                mX = x;
-                mY = y;
+
+            if(drawType==1) {
+                float dx = Math.abs(x - mX0);
+                float dy = Math.abs(y - mY0);
+                if (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
+
+                    mX = x;
+                    mY = y;
+                }
+            }
+            else if(drawType==4) {
+                float dx = Math.abs(x - mX);
+                float dy = Math.abs(y - mY);
+                if (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
+                    mPath.quadTo(mX, mY, (x + mX) / 2, (y + mY) / 2);
+                    mX = x;
+                    mY = y;
+                }
             }
         }
 
         private void touch_up() {
-            mPath.lineTo(mX, mY);
-            // commit the path to our offscreen
-            mCanvas.drawPath(mPath,  mPaint);
-            // kill this so we don't double draw
-            mPath.reset();
+            if(drawType==1) {
+
+            }
+            else if(drawType==4) {
+                mPath.lineTo(mX, mY);
+                // commit the path to our offscreen
+                mCanvas.drawPath(mPath, mPaint);
+                // kill this so we don't double draw
+                mPath.reset();
+            }
         }
 
         @Override
@@ -102,22 +137,20 @@ public class MainActivity extends AppCompatActivity {
                     drawType++;
                     if(drawType>4)
                         drawType=1;
+                    touch_start(x, y);
                     if(drawType==4) {
-                        touch_start(x, y);
+                        invalidate();
                     }
-                    invalidate();
                     break;
                 case MotionEvent.ACTION_MOVE:
+                    touch_move(x, y);
                     if(drawType==4) {
-                        touch_move(x, y);
+                        invalidate();
                     }
-                    invalidate();
                     break;
                 case MotionEvent.ACTION_UP:
-                    if(drawType==4) {
-                        touch_up();
-                    }
-                    invalidate();
+                    touch_up();
+                   invalidate();
                     break;
             }
             return true;
