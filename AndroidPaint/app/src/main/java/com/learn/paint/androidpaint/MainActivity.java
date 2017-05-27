d36061f25ba6d22f4f14e9c6fafc5b552f7d4931
Lei <yang_lei@mcafee.com>
2017-05-27 08:13:02 +0800

    pay 50 can change shape

--- AndroidPaint/app/src/main/java/com/learn/paint/androidpaint/MainActivity.java
+++ AndroidPaint/app/src/main/java/com/learn/paint/androidpaint/MainActivity.java
@@ -10,18 +10,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
     DrawingView dv ;
     private Paint mPaint;
-    private int drawType=0;
+    private int drawType=1;
+    private RadioButton rdbLine;
+    private RadioButton rdbRect;
+    private RadioButton rdbCircle;
+    private RadioButton rdbErase;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        dv = new DrawingView(this);
-        setContentView(dv);
-        mPaint = new Paint();
+    private void resetPaint()
+    {
         mPaint.setAntiAlias(true);
         mPaint.setDither(true);
         mPaint.setColor(Color.GREEN);
@@ -29,6 +31,80 @@ public class MainActivity extends AppCompatActivity {
         mPaint.setStrokeJoin(Paint.Join.ROUND);
         mPaint.setStrokeCap(Paint.Cap.ROUND);
         mPaint.setStrokeWidth(12);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        LinearLayout linearLayout = (LinearLayout)findViewById(R.id.buttomLayout);
+        this.rdbLine=(RadioButton) findViewById(R.id.rdbLine);
+        this.rdbRect=(RadioButton) findViewById(R.id.rdbRect);
+        this.rdbCircle=(RadioButton) findViewById(R.id.rdbCircle);
+        this.rdbErase=(RadioButton) findViewById(R.id.rdbErase);
+
+        rdbLine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawType=1;
+                rdbRect.setChecked(false);
+                rdbCircle.setChecked(false);
+                rdbErase.setChecked(false);
+            }
+        });
+        rdbRect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawType=2;
+                rdbLine.setChecked(false);
+                rdbCircle.setChecked(false);
+                rdbErase.setChecked(false);
+            }
+        });
+        rdbCircle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawType=3;
+                rdbLine.setChecked(false);
+                rdbRect.setChecked(false);
+                rdbErase.setChecked(false);
+            }
+        });
+        rdbErase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawType=4;
+                rdbLine.setChecked(false);
+                rdbRect.setChecked(false);
+                rdbCircle.setChecked(false);
+
+            }
+        });
+
+        dv = new DrawingView(this);
+if(linearLayout==null) {
+    setContentView(dv);
+}
+else {
+    linearLayout.addView(dv);
+    dv.width=200;
+    dv.height=200;
+}
+//*/
+
+        mPaint = new Paint();
+        /*
+        mPaint.setAntiAlias(true);
+        mPaint.setDither(true);
+        mPaint.setColor(Color.GREEN);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeJoin(Paint.Join.ROUND);
+        mPaint.setStrokeCap(Paint.Cap.ROUND);
+        mPaint.setStrokeWidth(12);
+        */
+        resetPaint();
     }
 
     public class DrawingView extends View {
@@ -56,6 +132,9 @@ public class MainActivity extends AppCompatActivity {
             mCanvas = new Canvas(mBitmap);
         }
 
+
+
+
         @Override
         protected void onDraw(Canvas canvas) {
             super.onDraw(canvas);
@@ -64,12 +143,15 @@ public class MainActivity extends AppCompatActivity {
 
 
               if(drawType==1) {
+                  resetPaint();
                   mCanvas.drawLine(mX0, mY0, mX, mY, mPaint);
               }
           else  if(drawType==2) {
+                  resetPaint();
                 mCanvas.drawRect(mX0, mY0, mX, mY, mPaint);
             }
               else  if(drawType==3) {
+                  resetPaint();
                   double r=Math.sqrt((mX-mX0)*(mX-mX0)+(mY-mY0)*(mY-mY0));
                   mCanvas.drawCircle(mX0, mY0,  (float) r, mPaint);
               }
@@ -142,9 +224,7 @@ public class MainActivity extends AppCompatActivity {
 
             switch (event.getAction()) {
                 case MotionEvent.ACTION_DOWN:
-                    drawType++;
-                    if(drawType>4)
-                        drawType=1;
+
                     touch_start(x, y);
                     if(drawType==4) {
                         invalidate();
